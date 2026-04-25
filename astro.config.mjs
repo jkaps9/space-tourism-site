2b0a4e5044eda2112ba079ab266f812433ee93c0
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-25 00:21:00 -0400

    undo github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jkaps9.github.io", //TODO: update me!
-	base: "/space-toursim-site",
+  site: "https://jkaps9.github.io/space-tourism-site", //TODO: update me!
   integrations: [],
 });
